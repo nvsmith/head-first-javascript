25cfec705ef8f9eb6f39e4f34d8a40f8fb070007
Nathan Smith <nathanielvs@gmail.com>
2022-08-21 22:36:13 -0600

    moved playlist script to external file

---
+++ chapter6/playlist.js
@@ -0,0 +1,11 @@
+window.onload = addSongs;
+
+function addSongs() {
+  var song1 = document.getElementById("song1");
+  var song2 = document.getElementById("song2");
+  var song3 = document.getElementById("song3");
+
+  song1.innerHTML = "March of the Pigs";
+  song2.innerHTML = "White Pony";
+  song3.innerHTML = "Busy People";
+}
