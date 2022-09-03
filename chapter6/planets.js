74055c89cb26df04ba43e2c7072423843db031fb
Nathan Smith <nathanielvs@gmail.com>
2022-09-02 21:28:00 -0600

    Move script to external file

--- chapter6/planets.js
+++ chapter6/planets.js
@@ -1,2 +1,19 @@
-var planet = document.getElementById("greenplanet");
-planet.innerHTML = "Danger, Will Robinson!"
+function init() {
+  const btn = document.getElementById("runJS");
+  btn.addEventListener("click", runScript);
+
+  function runScript() {
+    var planet = document.getElementById("greenplanet");
+    planet.innerHTML = "WARNING! Berserker Probes";
+    planet.setAttribute("class", "redtext");
+  }
+  document.que;
+
+  let logMessage = function () {
+    console.log("This message displays after 2 seconds");
+  };
+
+  setTimeout(logMessage, 2000);
+}
+
+window.onload = init;
