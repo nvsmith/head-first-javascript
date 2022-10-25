3f8317a695713dab0fc15c8898f0762e07c3e806
Nathan Smith <nathanielvs@gmail.com>
2022-10-25 10:01:47 -0600

    Passenger processing for No Fly and No Payment

--- chapter10/passengerCheck.js
+++ chapter10/passengerCheck.js
@@ -1,14 +1,15 @@
-var passengers = [
+const passengers = [
   { name: "Jack", paid: true },
   { name: "Cruella", paid: true },
   { name: "John", paid: false },
   { name: "Jane", paid: false },
 ];
 
+// Checks all passengers for a testable condition which will stall the plane's takeoff
 function processPassengers(passengers, testFunction) {
   // iterates through passenger list
   for (let i = 0; i < passengers.length; i++) {
-    // looks for a test condition among passengers
+    // applies a test condition for passengers
     if (testFunction(passengers[i])) {
       // for any true test condition, return false so plane can't leave
       return false;
@@ -16,12 +17,26 @@ function processPassengers(passengers, testFunction) {
   }
 }
 
+// Checks if a passenger is a No Fly
 function checkNoFlyList(passenger) {
   // returns true if Cruella is a passenger name
   return passenger.name === "Cruella";
 }
 
+// Checks if a passenger hasn't paid
 function checkNotPaid(passenger) {
   // returns true if any passenger hasn't paid
   return !passenger.paid;
+}
+
+// Processes passengers against No Fly condition
+let allCanFly = processPassengers(passengers, checkNoFlyList);
+if (!allCanFly) {
+  console.log("We have a no-fly passenger onboard.");
+}
+
+// Processes passengers against No Payment condition
+let allPaid = processPassengers(passengers, checkNotPaid);
+if (!allPaid) {
+  console.log("Not everyone has paid.");
 }
